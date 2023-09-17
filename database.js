0d9d5c28c0537af8f2a76ef188d4d72c3576c3c9
Jasper Nielsen <jasper.nielsen@hotmail.com>
2023-09-17 13:29:19 +0200

    evn implementering

--- database.js
+++ database.js
@@ -1,11 +1,13 @@
 import mysql from "mysql2";
+import "dotenv/config";
+const pEnv = process.env;
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  database: "music_Base",
-  password: "CPBBTS#1314a",
+  host: pEnv.MYSQL_HOST,
+  port: pEnv.MYSQL_PORT,
+  user: pEnv.MYSQL_USER,
+  database: pEnv.MYSQL_DATABASE,
+  password: pEnv.MYSQL_PASSWORD,
   multipleStatements: true,
 });
 
